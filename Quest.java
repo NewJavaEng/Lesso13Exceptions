2cfa3193513b8d601380d8a1232b63fa10ee23f7
NewJavaEng <32841342+NewJavaEng@users.noreply.github.com>
2017-11-18 20:40:44 +0300

    Add files via upload

---
+++ Quest.java
@@ -0,0 +1,25 @@
+package by.http.exception.athometwo;
+
+public class Quest {
+	private int qQ;
+
+	public Quest(int q) {
+		qQ = 12 / q;// 1
+	}
+
+	public int getQQ() {
+		return qQ;// 2
+	}
+
+	public static void main(String[] args) {
+		Quest quest = null;
+		try {
+			quest = new Quest(0);// 3
+		} catch (Exception e) {// 4
+		}
+		// fails in the next line, but if make Quest quest = new Quest (1) in line 15, then NPE is not showw
+		// or
+		// set 1 intead of 0 in line 17: quest = new Quest(1)
+		System.out.println(quest.getQQ());// 5
+	}
+}
